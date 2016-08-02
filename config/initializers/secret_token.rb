e3ec7f221233002a6b5917eb4391ca163db1a528
sefp-test <mail-for-rails-tutorial-sefp@mail.ru>
2016-08-02 17:59:48 +0300

    Initial demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'a977fa81c13886df45ade86f1fb6ac24aa63816a7cd7d45ce9033d483bd2cd447ec48ece2ddffa5f6efb3e5c67a498b97529993428921e3140bb6491bdd236e4'
